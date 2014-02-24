653d3722d147a6f5b541f1b8b9450cee32e8d86a
chrisgregan <chris@snozbot.com>
2014-02-24 13:53:27 +0000

    Renamed AnimationEventListener to AnimationListener

--- Assets/Fungus/Scripts/AnimationListener.cs
+++ Assets/Fungus/Scripts/AnimationListener.cs
@@ -4,7 +4,7 @@ using Fungus;
 
 // Listens for animation events
 // The string parameter specifies a method name on the active room class
-public class AnimationEventListener : MonoBehaviour 
+public class AnimationListener : MonoBehaviour 
 {
 	void OnAnimationEvent(string methodName)
 	{
