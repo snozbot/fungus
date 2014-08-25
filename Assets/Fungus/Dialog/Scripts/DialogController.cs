cf861a3a27714b1a918b42577b671913c8cbcd89
chrisgregan <chris@snozbot.com>
2014-08-25 10:58:55 +0100

    Added dialog controller class

---
+++ Assets/Fungus/Dialog/Scripts/DialogController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class DialogController : MonoBehaviour 
+{
+	public Button optionButton;
+	public GridLayoutGroup optionButtonGrid;
+	public Text storyText;
+	public Text nameText;
+	public Image leftImage;
+	public Image rightImage;
+
+	public UnityEvent testEvent;
+
+	// Write story text over time
+	// Show character image (with side, fade in?)
+	// Hide / Show canvas
+	// Show continue image
+	// Show one button
+	// Show button grid
+
+}
