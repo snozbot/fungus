604d52ecac9a8e590c7df91d514814310093d06b
chrisgregan <chrisgregan@gmail.com>
2014-08-26 17:20:14 +0100

    Custom editor for SetDialog

---
+++ Assets/Fungus/Dialog/Editor/SetDialog.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Rotorz.ReorderableList;
+
+namespace Fungus.Script
+{
+
+	[CustomEditor (typeof(SetDialog))]
+	public class SetDialogEditor : FungusCommandEditor
+	{
+		public override void DrawCommandGUI() 
+		{
+			SetDialog t = target as SetDialog;
+
+			EditorGUI.BeginChangeCheck();
+
+			DialogController dialogController = FungusCommandEditor.ObjectField<DialogController>(new GUIContent("Active Dialog", "Dialog to use when displaying Say command story text"), 
+			                                                                                      new GUIContent("<None>"),
+			                                                                                      t.dialogController);						                                         
+			if (EditorGUI.EndChangeCheck())
+			{
+				Undo.RecordObject(t, "Set Dialog");
+				t.dialogController = dialogController;
+			}			
+		}
+	}
+
+}
