dbae4165aa3644fdce4279c492488cff8fbc742e
chrisgregan <chris@snozbot.com>
2014-09-04 22:24:31 +0100

    Added Animator Parameter commands & fixed parallax

--- Assets/Fungus/Sprite/Scripts/Parallax.cs
+++ Assets/Fungus/Sprite/Scripts/Parallax.cs
@@ -5,28 +5,22 @@ namespace Fungus
 {
 	/**
 	 * Attach this component to a sprite object to apply a simple parallax scrolling effect.
-	 * The horizontal and vertical parallax offset is calculated based on the distance from the camera to the position of the parent Room.
-	 * The scale parallax is calculated based on the ratio of the camera size to the size of the Room. This gives a 'dolly zoom' effect.
-	 * Accelerometer based parallax may also be applied on devices that support it.
+	 * The horizontal and vertical parallax offset is calculated based on the distance from the camera to the position of the background sprite.
+	 * The scale parallax is calculated based on the ratio of the camera size to the size of the background sprite. This gives a 'dolly zoom' effect.
+	 * Accelerometer based parallax is also applied on devices that support it.
 	 */
 	public class Parallax : MonoBehaviour 
 	{
 		/**
+		 * The background sprite which this sprite is layered on top of.
+		 * The position of this sprite is used to calculate the parallax offset.
+		 */
+		public SpriteRenderer backgroundSprite;
+
+		/**
 		 * Scale factor for calculating the parallax offset.
 		 */
 		public Vector2 parallaxScale = new Vector2(0.25f, 0f);
-
-		/**
-		 * Scale factor when camera is zoomed out to show the full Room.
-		 * This will typically be set to 1 to show the sprite at normal size.
-		 */
-		public float zoomedOutScale = 1f;
-
-		/**
-		 * Scale factor when camera is fully zoomed in on Room.
-		 * Setting this to a value greater than 1 will give a 'dolly zoom' effect when zooming in.
-		 */
-		public float zoomedInScale = 1f;
 
 		/**
 		 * Scale factor for calculating parallax offset based on device accelerometer tilt angle.
@@ -35,7 +29,6 @@ namespace Fungus
 		public float accelerometerScale = 0.5f;
 
 		Vector3 startPosition;
-		// Vector3 startScale;
 
 		Vector3 acceleration;
 		Vector3 velocity;
@@ -44,20 +37,33 @@ namespace Fungus
 		{
 			// Store the starting position and scale of the sprite object
 			startPosition = transform.position;
-			// startScale = transform.localScale;
+
+			// Default to using parent sprite as background
+			if (backgroundSprite == null)
+			{
+				backgroundSprite = gameObject.GetComponentInParent<SpriteRenderer>();
+			}
 		}
 
 		void Update () 
 		{
+			if (backgroundSprite == null)
+			{
+				return;
+			}
+
 			Vector3 translation = Vector3.zero;
 
-			// Apply parallax translation based on camera position relative to Room
+			// Apply parallax translation based on camera position relative to background sprite
 			{
-				Vector3 a = startPosition;
+				Vector3 a = backgroundSprite.bounds.center;
 				Vector3 b = Camera.main.transform.position;
 				translation = (a - b);
 				translation.x *= parallaxScale.x;
 				translation.y *= parallaxScale.y;
+				translation.z = 0;
+
+				// TODO: Limit parallax offset to just outside the bounds of the background sprite
 			}
 
 			// Apply parallax translation based on device accelerometer
@@ -72,14 +78,6 @@ namespace Fungus
 			}
 
 			transform.position = startPosition + translation;
-
-			// Set new scale for sprite
-			/*
-			float roomSize = parentRoom.renderer.bounds.extents.y;
-			float t = Camera.main.orthographicSize / roomSize ;
-			float scale = Mathf.Lerp (zoomedInScale, zoomedOutScale, t);
-			transform.localScale = startScale * scale;
-			*/
 		}
 	}
 }
