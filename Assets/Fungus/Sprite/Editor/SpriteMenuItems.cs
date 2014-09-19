6e18bbd6191f96340ce34318af91b1b8cdfe1c24
chrisgregan <chris@snozbot.com>
2014-09-19 16:58:51 +0100

    Added Camera & Sprite menu items

---
+++ Assets/Fungus/Sprite/Editor/SpriteMenuItems.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace Fungus
+{
+
+	public class SpriteMenuItems 
+	{
+
+		[MenuItem("GameObject/Fungus/Sprite/ParallaxSprite")]
+		static void CreateBackground()
+		{
+			InstantiatePrefab("ParallaxSprite");
+		}
+
+		static void InstantiatePrefab(string prefabName)
+		{
+			GameObject prefab = Resources.LoadAssetAtPath("Assets/Fungus/Sprite/Prefabs/" + prefabName + ".prefab", typeof(GameObject)) as GameObject;
+			if (prefab != null)
+			{
+				GameObject go = GameObject.Instantiate(prefab) as GameObject;
+				go.name = prefabName;
+			}
+		}
+
+	}
+
+}
