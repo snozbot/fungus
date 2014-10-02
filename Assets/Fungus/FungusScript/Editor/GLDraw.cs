2041ac1c3e2bff6b3225292ab019f71b9cd176ef
chrisgregan <chris@snozbot.com>
2014-10-02 14:47:37 +0100

    Fixed arrow head rendering

--- Assets/Fungus/FungusScript/Editor/GLDraw.cs
+++ Assets/Fungus/FungusScript/Editor/GLDraw.cs
@@ -247,10 +247,10 @@ public class GLDraw
 
         DrawBezier(start, tangentA, end, tangentB, color, width, segments);
 
-		Vector2 pA = CubeBezier(start, tangentA, end, tangentB, 0.7f);
-		Vector2 pB = CubeBezier(start, tangentA, end, tangentB, 0.8f);
+		Vector2 pA = CubeBezier(start, tangentA, end, tangentB, 0.4f);
+		Vector2 pB = CubeBezier(start, tangentA, end, tangentB, 0.5f);
 
-		float arrowHeadSize = 10;
+		float arrowHeadSize = 5;
 		
 		Vector2 arrowPosA = pB;
 		Vector2 arrowPosB = arrowPosA;
@@ -266,9 +266,9 @@ public class GLDraw
 		arrowPosC.y += dir.x * arrowHeadSize;
 		arrowPosC -= dir * arrowHeadSize;
 
-		DrawLine(arrowPosA, arrowPosB, color, 1);
-		DrawLine(arrowPosA, arrowPosC, color, 1);
-		DrawLine(arrowPosB, arrowPosC, color, 1);
+		DrawLine(arrowPosA, arrowPosB, color, 1.025f);
+		DrawLine(arrowPosA, arrowPosC, color, 1.025f);
+		// DrawLine(arrowPosB, arrowPosC, color, 1);
 	}
 
     public static void DrawBezier(Vector2 start, Vector2 startTangent, Vector2 end, Vector2 endTangent, Color color, float width)
