f63bae60e04eaffe1e4aed8bcd0c6161100e9ddc
Kéven Boily <kboily@hibernum.com>
2014-10-06 15:45:26 -0400

    [feature] Added priority to CommandListAttribute, allowing to override menu items with custom command implementation
    This example would replace the "Dialog/Say" option in the menu with a command of type SayWithKey instead of the stock Say command
    [CommandInfo("Dialog",
                 "Say",
                 "Writes a line of story text to the dialog using a key to pull from localization document.",
                 1)]
    
    public partial class SayWithKey : Say, ILocalizable
    {
    [...]
    }

--- Assets/Fungus/FungusScript/Editor/CommandListAdaptor.cs
+++ Assets/Fungus/FungusScript/Editor/CommandListAdaptor.cs
@@ -337,6 +337,28 @@ namespace Fungus
 
 			// Build menu list
 			List<System.Type> menuTypes = EditorExtensions.FindDerivedTypes(typeof(Command)).ToList();
+
+			List<KeyValuePair<System.Type,CommandInfoAttribute>> filteredAttributes = GetFilteredCommandInfoAttribute(menuTypes);
+
+			foreach(var keyPair in filteredAttributes)
+			{
+				SetCommandOperation commandOperation = new SetCommandOperation();
+				
+				commandOperation.sequence = sequence;
+				commandOperation.commandType = keyPair.Key;
+				commandOperation.index = index;
+				
+				commandMenu.AddItem (new GUIContent (keyPair.Value.Category + "/" + keyPair.Value.CommandName), 
+				                     false, Callback, commandOperation);
+			}
+
+			commandMenu.ShowAsContext();
+		}
+
+		List<KeyValuePair<System.Type,CommandInfoAttribute>> GetFilteredCommandInfoAttribute(List<System.Type> menuTypes)
+		{
+			Dictionary<string, KeyValuePair<System.Type,CommandInfoAttribute>> filteredAttributes = new Dictionary<string, KeyValuePair<Type, CommandInfoAttribute>>();
+			
 			foreach(System.Type type in menuTypes)
 			{
 				object[] attributes = type.GetCustomAttributes(false);
@@ -345,21 +367,23 @@ namespace Fungus
 					CommandInfoAttribute infoAttr = obj as CommandInfoAttribute;
 					if (infoAttr != null)
 					{
-						SetCommandOperation commandOperation = new SetCommandOperation();
-
-						commandOperation.sequence = sequence;
-						commandOperation.commandType = type;
-						commandOperation.index = index;
-
-						commandMenu.AddItem (new GUIContent (infoAttr.Category + "/" + infoAttr.CommandName), 
-						                     false, Callback, commandOperation);
+						string dictionnaryName = string.Format("{0}/{1}",infoAttr.Category,infoAttr.CommandName);
+						
+						int exisitingItemPriotiry = -1;
+						if(filteredAttributes.ContainsKey(dictionnaryName))
+							exisitingItemPriotiry = filteredAttributes[dictionnaryName].Value.Priority;
+						
+						if(infoAttr.Priority > exisitingItemPriotiry)
+						{
+							KeyValuePair<System.Type, CommandInfoAttribute> keyValuePair = new KeyValuePair<Type, CommandInfoAttribute>(type,infoAttr);
+							filteredAttributes[dictionnaryName] = keyValuePair;
+						}
 					}
 				}
 			}
-
-			commandMenu.ShowAsContext();
+			return filteredAttributes.Values.ToList<KeyValuePair<System.Type,CommandInfoAttribute>>();
 		}
-
+		
 		void Callback(object obj)
 		{
 			SetCommandOperation commandOperation = obj as SetCommandOperation;
