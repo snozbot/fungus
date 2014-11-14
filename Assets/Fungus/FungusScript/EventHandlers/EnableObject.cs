9a05a55a22ad8e308f49b93e2c0e2f4db6b611c4
chrisgregan <chris@snozbot.com>
2014-11-14 11:57:29 +0000

    Added Enable Object event handler

---
+++ Assets/Fungus/FungusScript/EventHandlers/EnableObject.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Fungus
+{
+	[EventHandlerInfo("Scripting",
+	                  "Enable Object",
+	                  "The sequence will execute when the owner game object is enabled.")]
+	public class EnableObject : EventHandler
+	{	
+		protected virtual void OnEnable()
+		{
+			ExecuteSequence();
+		}
+	}
+}
