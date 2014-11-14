dccf5dc5bffeaa90f9b850021702d22d8506f3e2
chrisgregan <chris@snozbot.com>
2014-11-14 16:31:42 +0000

    Added Send Message command and Receive Message event handler
    Messages are a simple way to notify other sequences to start executing
    when something happens.
    You can specify if the message should only be sent to the same Fungus
    Script, or to all Fungus Scripts in the scene.

---
+++ Assets/Fungus/FungusScript/EventHandlers/ReceiveMessage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fungus
+{
+	[EventHandlerInfo("",
+	                  "Receive Message",
+	                  "The sequence will execute when the specified message is received from a SendFungusMessage command.")]
+	public class ReceiveMessage : EventHandler 
+	{
+		public string message;
+
+		public void OnSendFungusMessage(string message)
+		{
+			if (this.message == message)
+			{
+				ExecuteSequence();
+			}
+		}
+	}
+
+}
