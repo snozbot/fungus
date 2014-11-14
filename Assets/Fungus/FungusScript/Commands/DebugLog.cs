55f6a9d15ebc69dc0a075e59e56d5da7600264ca
chrisgregan <chris@snozbot.com>
2014-11-14 14:25:02 +0000

    Added Debug Log command

---
+++ Assets/Fungus/FungusScript/Commands/DebugLog.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fungus
+{
+	[CommandInfo("Scripting", 
+	             "Debug Log", 
+	             "Writes a log message to the debug console.")]
+	public class DebugLog : Command 
+	{
+		public enum DebugLogType
+		{
+			Info,
+			Warning,
+			Error
+		}
+
+		public DebugLogType logType;
+
+		public StringData logMessage;
+
+		public override void OnEnter ()
+		{
+			switch (logType)
+			{
+			case DebugLogType.Info:
+				Debug.Log(logMessage.Value);
+				break;
+			case DebugLogType.Warning:
+				Debug.LogWarning(logMessage.Value);
+				break;
+			case DebugLogType.Error:
+				Debug.LogError(logMessage.Value);
+				break;
+			}
+
+			Continue();
+		}
+
+		public override string GetSummary()
+		{
+			return logMessage.GetDescription();
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(235, 191, 217, 255);
+		}
+	}
+
+}
