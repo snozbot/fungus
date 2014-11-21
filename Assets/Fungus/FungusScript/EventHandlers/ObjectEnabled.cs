86a6038e95fcbf268a4f500e45651bc619905b92
chrisgregan <chris@snozbot.com>
2014-11-21 14:58:07 +0000

    Updated ObjectEnabled command description

--- Assets/Fungus/FungusScript/EventHandlers/ObjectEnabled.cs
+++ Assets/Fungus/FungusScript/EventHandlers/ObjectEnabled.cs
@@ -7,7 +7,7 @@ namespace Fungus
 {
 	[EventHandlerInfo("",
 	                  "Object Enabled",
-	                  "The sequence will execute when the owner game object is enabled.")]
+	                  "The sequence will execute when the Fungus Script game object is enabled.")]
 	public class ObjectEnabled : EventHandler
 	{	
 		protected virtual void OnEnable()
