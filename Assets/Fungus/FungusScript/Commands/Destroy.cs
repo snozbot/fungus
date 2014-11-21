cc49c36917f26b80eb2dc01c16857bcbad1137ae
chrisgregan <chris@snozbot.com>
2014-11-21 14:54:54 +0000

    Added Destroy command

---
+++ Assets/Fungus/FungusScript/Commands/Destroy.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Fungus
+{
+	[CommandInfo("Scripting", 
+	             "Destroy", 
+	             "Destroys a specified game object in the scene.")]
+	public class Destroy : Command
+	{	
+		[Tooltip("Reference to game object to destroy")]
+		public GameObject targetGameObject;
+
+		public override void OnEnter()
+		{
+			if (targetGameObject != null)
+			{
+				Destroy(targetGameObject);
+			}
+
+			Continue();
+		}
+
+		public override string GetSummary()
+		{
+			if (targetGameObject == null)
+			{
+				return "Error: No game object selected";
+			}
+
+			return targetGameObject.name;
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(235, 191, 217, 255);
+		}
+	}
+
+}
