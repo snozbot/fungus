e066f9c2d88e2e0298e340df4c9e1e360a898c9f
chrisgregan <chris@snozbot.com>
2014-12-11 13:23:35 +0000

    Fixed DragCompleted handlers firing incorrectly.

--- Assets/Fungus/Sprite/Scripts/EventHandlers/DragCompleted.cs
+++ Assets/Fungus/Sprite/Scripts/EventHandlers/DragCompleted.cs
@@ -12,39 +12,46 @@ namespace Fungus
 	{	
 		public Draggable2D draggableObject;
 		public Collider2D targetObject;
-
+		
 		// There's no way to poll if an object is touching another object, so
 		// we have to listen to the callbacks and track the touching state ourselves.
 		bool overTarget = false;
-
+		
 		public virtual bool IsOverTarget()
 		{
 			return overTarget;
 		}
-
+		
 		public virtual void OnDragEntered(Draggable2D draggableObject, Collider2D targetObject)
 		{
 			if (this.targetObject != null &&
+			    draggableObject == this.draggableObject &&
 			    targetObject == this.targetObject)
 			{
 				overTarget = true;
 			}
 		}
-
+		
 		public virtual void OnDragExited(Draggable2D draggableObject, Collider2D targetObject)
 		{
 			if (this.targetObject != null &&
+			    draggableObject == this.draggableObject &&
 			    targetObject == this.targetObject)
 			{
 				overTarget = false;
 			}
 		}
-
+		
 		public virtual void OnDragCompleted(Draggable2D draggableObject)
 		{
 			if (draggableObject == this.draggableObject &&
 			    overTarget)
 			{
+				// Assume that the player will have to do perform another drag and drop operation
+				// to complete the drag again. This is necessary because we don't get an OnDragExited if the
+				// draggable object is set to be inactive.
+				overTarget = false;
+				
 				ExecuteSequence();
 			}
 		}
