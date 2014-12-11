717b10658d7b5d1f2665001e26420d81c76b19ef
chrisgregan <chris@snozbot.com>
2014-12-11 15:19:31 +0000

    Added Reset Anim Trigger command

---
+++ Assets/Fungus/Animation/Scripts/Commands/ResetAnimTrigger.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Fungus
+{
+	[CommandInfo("Animation", 
+	             "Reset Anim Trigger", 
+	             "Resets a trigger parameter on an Animator component.")]
+	public class ResetAnimTrigger : Command 
+	{
+		[Tooltip("Reference to an Animator component in a game object")]
+		public Animator animator;
+
+		[Tooltip("Name of the trigger Animator parameter that will be reset")]
+		public string parameterName;
+
+		public override void OnEnter()
+		{
+			if (animator != null)
+			{
+				animator.ResetTrigger(parameterName);
+			}
+
+			Continue();
+		}
+
+		public override string GetSummary()
+		{
+			if (animator == null)
+			{
+				return "Error: No animator selected";
+			}
+
+			return animator.name + " (" + parameterName + ")";
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(170, 204, 169, 255);
+		}
+	}
+
+}
