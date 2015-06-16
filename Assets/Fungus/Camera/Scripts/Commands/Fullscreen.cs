40f2c61a852190cb98a8605101db141476ad4e17
chrisgregan <chris@fungusgames.com>
2015-06-16 14:41:42 +0100

    Added Fullscreen command
     https://trello.com/c/aA5GQlua

---
+++ Assets/Fungus/Camera/Scripts/Commands/Fullscreen.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Fungus
+{
+	[CommandInfo("Camera", 
+	             "Fullscreen", 
+	             "Sets the application to fullscreen, windowed or toggles the current state.")]
+	[AddComponentMenu("")]
+	public class Fullscreen : Command 
+	{
+		public enum FullscreenMode
+		{
+			Toggle,
+			Fullscreen,
+			Windowed
+		}
+
+		public FullscreenMode fullscreenMode;
+
+		public override void OnEnter()
+		{
+			switch (fullscreenMode)
+			{
+			case FullscreenMode.Toggle:
+				Screen.fullScreen = !Screen.fullScreen;
+				break;
+			case FullscreenMode.Fullscreen:
+				Screen.fullScreen = true;
+				break;
+			case FullscreenMode.Windowed:
+				Screen.fullScreen = false;
+				break;
+			}
+
+			Debug.Log("Fullscreen:" + fullscreenMode);
+			
+			Continue();
+		}
+
+		public override string GetSummary()
+		{
+			return fullscreenMode.ToString();
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(216, 228, 170, 255);
+		}
+	}
+
+}
