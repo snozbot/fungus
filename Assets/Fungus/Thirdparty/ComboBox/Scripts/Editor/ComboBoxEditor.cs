c66d29b3be5ad11598b24885364304c9e5593e1e
chrisgregan <chris@fungusgames.com>
2015-08-04 11:50:51 +0100

    Added ComboBox to Thirdparty folder

---
+++ Assets/Fungus/Thirdparty/ComboBox/Scripts/Editor/ComboBoxEditor.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ComboBox))]
+public class ComboBoxEditor : Editor 
+{
+	public override void OnInspectorGUI()
+	{
+		var comboBoxGO = target as ComboBox;
+
+		var allowUpdate = comboBoxGO.transform.Find("Button") != null;
+
+		if (allowUpdate)
+			comboBoxGO.UpdateGraphics();
+		
+		EditorGUI.BeginChangeCheck();
+		DrawDefaultInspector();
+		if (EditorGUI.EndChangeCheck())
+		{
+			if (Application.isPlaying)
+			{
+				comboBoxGO.HideFirstItem = comboBoxGO.HideFirstItem;
+				comboBoxGO.Interactable = comboBoxGO.Interactable;
+			}
+			else
+				if (allowUpdate)
+					comboBoxGO.RefreshSelected();
+		}
+	}
+}
+
+public class ComboBoxMenuItem
+{
+	[MenuItem("GameObject/UI/ComboBox")]
+	public static void CreateComboBox()
+	{
+		var canvas = Object.FindObjectOfType<Canvas>();
+		var canvasGO = canvas == null ? null : canvas.gameObject;
+		if (canvasGO == null)
+		{
+			canvasGO = new GameObject("Canvas");
+			canvas = canvasGO.AddComponent<Canvas>();
+			canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+			canvasGO.AddComponent<CanvasScaler>();
+			canvasGO.AddComponent<GraphicRaycaster>();
+		}
+		var eventSystem = Object.FindObjectOfType<EventSystem>();
+		var eventSystemGO = eventSystem == null ? null : eventSystem.gameObject;
+		if (eventSystemGO == null)
+		{
+			eventSystemGO = new GameObject("EventSystem");
+			eventSystem = eventSystemGO.AddComponent<EventSystem>();
+			eventSystemGO.AddComponent<StandaloneInputModule>();
+			eventSystemGO.AddComponent<TouchInputModule>();
+		}
+		var comboBoxGO = new GameObject("ComboBox");
+		comboBoxGO.transform.SetParent(canvasGO.transform, false);
+		var rTransform = comboBoxGO.AddComponent<RectTransform>();
+		rTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 160);
+		rTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 30);
+		for (var i = 0; i < Selection.objects.Length; i++)
+		{
+			var selected = Selection.objects[i] as GameObject;
+			var hierarchyItem = selected.transform;
+			canvas = null;
+			while (hierarchyItem != null && (canvas = hierarchyItem.GetComponent<Canvas>()) == null)
+				hierarchyItem = hierarchyItem.parent;
+			if (canvas != null)
+			{
+				comboBoxGO.transform.SetParent(selected.transform, false);
+				break;
+			}
+		}
+		rTransform.anchoredPosition = Vector2.zero;
+		var comboBox = comboBoxGO.AddComponent<ComboBox>();
+		LoadAssets();
+		comboBox.Sprite_UISprite = Sprite_UISprite;
+		comboBox.Sprite_Background = Sprite_Background;
+		comboBox.CreateControl();
+		Selection.activeGameObject = comboBoxGO;
+	}
+
+	private static Sprite Sprite_UISprite;
+	private static Sprite Sprite_Background;
+	public static void LoadAssets()
+	{
+		while (Sprite_UISprite == null || Sprite_Background == null)
+		{
+			var sprites = Resources.FindObjectsOfTypeAll<Sprite>();
+			foreach (var sprite in sprites)
+				switch (sprite.name)
+				{
+					case "UISprite":
+						Sprite_UISprite = sprite;
+						break;
+					case "Background":
+						Sprite_Background = sprite;
+						break;
+				}
+			if (Sprite_UISprite == null || Sprite_Background == null)
+				AssetDatabase.LoadAllAssetsAtPath("Resources/unity_builtin_extra");
+		}
+	}
+}
---
+++ Assets/Fungus/Thirdparty/ComboBox/Scripts/TestComboBox.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class TestComboBox : MonoBehaviour 
+{
+	public ComboBox comboBox;
+	public Sprite image;
+
+	private void Start() 
+	{
+		var itemMakeBig = new ComboBoxItem("Make me big!");
+		var itemMakeNormal = new ComboBoxItem("Normal", image, true);
+		var itemMakeSmall = new ComboBoxItem("Make me small!");
+		itemMakeBig.OnSelect += () =>
+		{
+			comboBox.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 180);
+			comboBox.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 40);
+			comboBox.UpdateGraphics();
+			itemMakeBig.Caption = "Big";
+			itemMakeBig.IsDisabled = true;
+			itemMakeNormal.Caption = "Make me normal!";
+			itemMakeNormal.IsDisabled = false;
+			itemMakeSmall.Caption = "Make me small!";
+			itemMakeSmall.IsDisabled = false;
+		};
+		itemMakeNormal.OnSelect += () =>
+		{
+			comboBox.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 160);
+			comboBox.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 30);
+			comboBox.UpdateGraphics();
+			itemMakeBig.Caption = "Make me big!";
+			itemMakeBig.IsDisabled = false;
+			itemMakeNormal.Caption = "Normal";
+			itemMakeNormal.IsDisabled = true;
+			itemMakeSmall.Caption = "Make me small!";
+			itemMakeSmall.IsDisabled = false;
+		};
+		itemMakeSmall.OnSelect += () =>
+		{
+			comboBox.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 160);
+			comboBox.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 20);
+			comboBox.UpdateGraphics();
+			itemMakeBig.Caption = "Make me big!";
+			itemMakeBig.IsDisabled = false;
+			itemMakeNormal.Caption = "Make me normal!";
+			itemMakeNormal.IsDisabled = false;
+			itemMakeSmall.Caption = "Small";
+			itemMakeSmall.IsDisabled = true;
+		};
+		comboBox.AddItems(itemMakeBig, itemMakeNormal, itemMakeSmall);
+		comboBox.SelectedIndex = 1;
+		comboBox.OnSelectionChanged += (int index) =>
+		{
+			Camera.main.backgroundColor = new Color32((byte)Random.Range(0, 256), (byte)Random.Range(0, 256), (byte)Random.Range(0, 256), 255);
+		};
+	}
+}
