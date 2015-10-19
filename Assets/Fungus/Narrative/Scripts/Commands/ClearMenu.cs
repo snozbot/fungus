8a25fe5a2e00eab74900d6987f7e07378b0001aa
chrisgregan <chris@fungusgames.com>
2015-10-19 13:50:04 +0100

    Added Clear Menu command #300

---
+++ Assets/Fungus/Narrative/Scripts/Commands/ClearMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fungus
+{
+
+	[CommandInfo("Narrative",
+	             "Clear Menu",
+	             "Clears the options from a menu dialogue")]
+	public class ClearMenu : Command 
+	{
+		[Tooltip("Menu Dialog to clear the options on")]
+		public MenuDialog menuDialog;
+
+		public override void OnEnter()
+		{
+			menuDialog.Clear();
+
+			Continue();
+		}
+
+		public override string GetSummary()
+		{
+			if (menuDialog == null)
+			{
+				return "Error: No menu dialog object selected";
+			}
+			
+			return menuDialog.name;
+		}
+		
+		public override Color GetButtonColor()
+		{
+			return new Color32(184, 210, 235, 255);
+		}
+	}
+
+}
--- Assets/Fungus/Narrative/Scripts/MenuDialog.cs
+++ Assets/Fungus/Narrative/Scripts/MenuDialog.cs
@@ -66,7 +66,7 @@ namespace Fungus
 			Canvas.ForceUpdateCanvases();
 		}
 
-		protected virtual void Clear()
+		public virtual void Clear()
 		{
 			StopAllCoroutines();
 
