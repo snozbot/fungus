6dd7c722eb021fdba599693a3e1b20f5453e31e3
chrisgregan <chris@fungusgames.com>
2016-01-22 13:32:31 +0000

    Added SetDraggable2D command #191

---
+++ Assets/Fungus/Sprite/Scripts/Commands/SetDraggable.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Fungus
+{
+	[CommandInfo("Sprite",
+				 "Set Draggable 2D",
+				 "Sets a Draggable2D component to be draggable / non-draggable.")]
+	[AddComponentMenu("")]
+	public class SetDraggable2D : Command
+	{      
+		[Tooltip("Reference to Draggable2D component on a gameobject")]
+		public Draggable2D targetDraggable2D;
+
+		[Tooltip("Set to true to enable the component")]
+		public BooleanData activeState;
+
+		public override void OnEnter() 
+		{
+			if (targetDraggable2D != null)         
+			{
+				targetDraggable2D.dragEnabled = activeState.Value;     
+			}
+
+			Continue();
+		}
+
+		public override string GetSummary()
+		{
+			if (targetDraggable2D == null)         
+			{
+				return "Error: No Draggable2D component selected";     
+			}
+
+			return targetDraggable2D.gameObject.name;
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(235, 191, 217, 255);
+		}
+	}
+
+}
