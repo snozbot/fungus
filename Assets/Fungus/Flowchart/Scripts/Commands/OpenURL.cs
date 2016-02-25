3c7be2df9bec82f480eca5af19a69654656c5840
chrisgregan <chris@fungusgames.com>
2016-02-25 22:32:08 +0000

    Open URL command #382

---
+++ Assets/Fungus/Flowchart/Scripts/Commands/OpenURL.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+namespace Fungus
+{
+
+	[CommandInfo("Scripting",
+				 "Open URL",
+				 "Opens the specified URL in the browser.")]
+	public class LinkToWebsite : Command
+	{
+		[Tooltip("URL to open in the browser")]
+		public string url;
+
+		public override void OnEnter()
+		{
+			Application.OpenURL(url);
+
+			Continue();
+		}
+
+		public override string GetSummary()
+		{
+			return url;
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(235, 191, 217, 255);
+		}
+	}
+
+}
