f05b9bcef1bade4c1359b6e954022bb5a0c2cd77
chrisgregan <chris@fungusgames.com>
2016-03-19 01:23:16 -0700

    Destroy command can use variable

--- Assets/Fungus/Flowchart/Scripts/Commands/Destroy.cs
+++ Assets/Fungus/Flowchart/Scripts/Commands/Destroy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -8,16 +9,20 @@ namespace Fungus
 	             "Destroy", 
 	             "Destroys a specified game object in the scene.")]
 	[AddComponentMenu("")]
-	public class Destroy : Command
+	public class Destroy : Command, ISerializationCallbackReceiver
 	{	
+		#region Obsolete Properties
+		[HideInInspector] [FormerlySerializedAs("targetGameObject")] public GameObject targetGameObjectOLD;
+		#endregion
+
 		[Tooltip("Reference to game object to destroy")]
-		public GameObject targetGameObject;
+		public GameObjectData _targetGameObject;
 
 		public override void OnEnter()
 		{
-			if (targetGameObject != null)
+			if (_targetGameObject.Value != null)
 			{
-				Destroy(targetGameObject);
+				Destroy(_targetGameObject.Value);
 			}
 
 			Continue();
@@ -25,17 +30,33 @@ namespace Fungus
 
 		public override string GetSummary()
 		{
-			if (targetGameObject == null)
+			if (_targetGameObject.Value == null)
 			{
 				return "Error: No game object selected";
 			}
 
-			return targetGameObject.name;
+			return _targetGameObject.Value.name;
 		}
 
 		public override Color GetButtonColor()
 		{
 			return new Color32(235, 191, 217, 255);
+		}
+
+		//
+		// ISerializationCallbackReceiver implementation
+		//
+
+		public virtual void OnBeforeSerialize()
+		{}
+
+		public virtual void OnAfterDeserialize()
+		{
+			if (targetGameObjectOLD != null)
+			{
+				_targetGameObject.Value = targetGameObjectOLD;
+				targetGameObjectOLD = null;
+			}
 		}
 	}
 
