035603a529244d5974551636d1a29831991522b3
Chris Gregan <chrisgregan@gmail.com>
2016-05-04 09:30:52 +0100

    Added HelloWorld Lua example

---
+++ Assets/FungusExamples/Lua/Scripts/InfoText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fungus
+{
+
+    /// <summary>
+    /// Displays information text at the top left of the screen.
+    /// </summary>
+    public class InfoText : MonoBehaviour 
+    {
+        [Tooltip("The information text to display")]
+        [TextArea(10, 10)]
+        public string info = "";
+    	
+    	void OnGUI() 
+        {
+            Rect rect = new Rect(0,0, Screen.width / 2, Screen.height);
+
+            GUI.Label(rect, info);
+    	}
+    }
+
+}
