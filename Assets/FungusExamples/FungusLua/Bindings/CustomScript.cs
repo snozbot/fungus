7f864e539a295b68d342743a1db876de50500a11
Chris Gregan <chrisgregan@gmail.com>
2016-05-05 07:20:34 +0100

    Added Bindings/CustomScript example scene

---
+++ Assets/FungusExamples/FungusLua/Bindings/CustomScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomScript : MonoBehaviour 
+{
+    public string myString = "";
+
+    public float timeToWait = 5f;
+
+    public void MyFunction()
+    {
+        Debug.Log("Called my function");
+    }
+
+    public IEnumerator MyCoroutine()
+    {
+        Debug.Log("Called my coroutine");
+
+        yield return new WaitForSeconds(timeToWait);
+
+        Debug.Log("Coroutine finished");
+    }
+
+}
