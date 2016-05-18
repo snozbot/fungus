f80f651010e9b60b5ee631d4fbed558a643a3d7a
Funky Ape <funky.monkey.ape@gmail.com>
2016-05-18 20:31:43 +0200

    Added ignoreTimeScale parameter to iTween commands

--- Assets/Fungus/iTween/Scripts/Commands/ShakePosition.cs
+++ Assets/Fungus/iTween/Scripts/Commands/ShakePosition.cs
@@ -40,7 +40,8 @@ namespace Fungus
 			tweenParams.Add("time", _duration.Value);
 			tweenParams.Add("easetype", easeType);
 			tweenParams.Add("looptype", loopType);
-			tweenParams.Add("isLocal", isLocal);
+            tweenParams.Add("ignoretimescale", ignoreTimeScale);
+            tweenParams.Add("isLocal", isLocal);
 			tweenParams.Add("oncomplete", "OniTweenComplete");
 			tweenParams.Add("oncompletetarget", gameObject);
 			tweenParams.Add("oncompleteparams", this);
