8be0cad328a7de8cba1f7fe673743b50d3ab4df4
Funky Ape <funky.monkey.ape@gmail.com>
2016-05-18 13:34:04 +0200

    Added new EventHandler for FlowChart based on ExecuteHandler

---
+++ Assets/Fungus/Flowchart/Scripts/EventHandlers/ExecuteEventHandler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+[EventHandlerInfo("", "On Execute", "Executes on a default Unity event")]
+public class ExecuteEventHandler : EventHandler
+{
+    public enum ExecuteHandlerContext
+    {
+        Self,
+        Static
+    }
+
+    public ExecuteHandler.ExecuteMethod executeEvent;
+
+    [Tooltip("Choose target for event listening")]
+    public ExecuteHandlerContext context;
+
+    [Tooltip("Target gameObject if context is Static")]
+    [HideInInspector]
+    public GameObjectData targetObject;
+
+    void Awake()
+    {
+        if (context == ExecuteHandlerContext.Self)
+        {
+            AddEventHandler(this.gameObject, executeEvent);
+        }
+        else if (context == ExecuteHandlerContext.Static)
+        {
+            AddEventHandler(targetObject.Value, executeEvent);
+        }
+    }
+
+    private void AddEventHandler(GameObject target, ExecuteHandler.ExecuteMethod method)
+    {
+        if (target != null)
+        {
+            var executeHandler = target.AddComponent<ExecuteHandler>();
+            executeHandler.executeMethods = method;
+            executeHandler.onExecute = (args) =>
+            {
+                this.ExecuteBlock();
+            };
+        }
+    }
+}
