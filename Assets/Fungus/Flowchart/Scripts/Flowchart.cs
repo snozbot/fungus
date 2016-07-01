be7109ba56a655f9194d1b6a0649c917fbdd666f
Christopher <chrisgregan@gmail.com>
2016-07-01 08:11:21 +0100

    Removed redundant copyright notice

--- Assets/Fungus/Flowchart/Scripts/Flowchart.cs
+++ Assets/Fungus/Flowchart/Scripts/Flowchart.cs
@@ -3,12 +3,6 @@
  * It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
  */
 
-/**
- * Copyright (c) 2014 Fungus Ltd
- * This code is part of the Fungus library (http://fungusgames.com) created by Chris Gregan (http://twitter.com/gofungus).
- * It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
- */
-
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
