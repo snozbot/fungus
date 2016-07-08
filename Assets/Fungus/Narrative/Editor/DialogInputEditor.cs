5382f84f2e766a5001fa56ffe513a24c0ebf0bff
Christopher <chrisgregan@gmail.com>
2016-07-08 18:12:41 +0100

    Fix for compile error for folks upgrading using the Fungus 3 unitypackage

---
+++ Assets/Fungus/Narrative/Editor/DialogInputEditor.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Dummy class to avoid compile errors for users upgrading from Fungus 2 to Fungus 3 via unitypackage.
+/// </summary>
+public class DialogInputEditor
+{}
