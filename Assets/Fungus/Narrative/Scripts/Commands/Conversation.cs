f79c04f8e02576a15d7bc4bcaebc62644373353f
Christopher <chrisgregan@gmail.com>
2016-07-26 15:49:38 +0100

    Added Conversation command

---
+++ Assets/Fungus/Narrative/Scripts/Commands/Conversation.cs
@@ -0,0 +1,50 @@
+/**
+ * This code is part of the Fungus library (http://fungusgames.com) maintained by Chris Gregan (http://twitter.com/gofungus).
+ * It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
+ */
+
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using System.Collections;
+
+namespace Fungus
+{
+	[CommandInfo("Narrative", 
+	             "Conversation", 
+	             "Do multiple say and portrait commands in a single block of text. Format is: [character] [portrait] [stage position] [: Story text]")]
+	[AddComponentMenu("")]
+	[ExecuteInEditMode]
+	public class Conversation : Command
+	{
+        public StringDataMulti conversationText;
+
+        public ConversationManager conversationManager = new ConversationManager();
+
+        protected virtual void Start()
+        {
+            conversationManager.PopulateCharacterCache();
+        }
+
+		public override void OnEnter()
+		{
+            StartCoroutine(DoConversation());
+		}
+
+        protected virtual IEnumerator DoConversation()
+        {
+            yield return StartCoroutine(conversationManager.DoConversation(conversationText.Value));
+
+            Continue();
+        }
+
+		public override string GetSummary()
+		{
+            return conversationText.Value;
+		}
+
+		public override Color GetButtonColor()
+		{
+			return new Color32(184, 210, 235, 255);
+		}
+	}
+}
