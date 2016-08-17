5791e3a451b16c4e7cd60650b701f29c45400f43
Christopher <chrisgregan@gmail.com>
2016-08-17 14:03:57 +0100

    Fixed missing .Value on _parameterName property.

--- Assets/Fungus/Animation/Scripts/Commands/ResetAnimTrigger.cs
+++ Assets/Fungus/Animation/Scripts/Commands/ResetAnimTrigger.cs
@@ -10,65 +10,65 @@ using System.Collections;
 
 namespace Fungus
 {
-	[CommandInfo("Animation", 
-	             "Reset Anim Trigger", 
-	             "Resets a trigger parameter on an Animator component.")]
-	[AddComponentMenu("")]
-	[ExecuteInEditMode]
-	public class ResetAnimTrigger : Command
-	{
-		[Tooltip("Reference to an Animator component in a game object")]
-		public AnimatorData _animator;
+    [CommandInfo("Animation", 
+                 "Reset Anim Trigger", 
+                 "Resets a trigger parameter on an Animator component.")]
+    [AddComponentMenu("")]
+    [ExecuteInEditMode]
+    public class ResetAnimTrigger : Command
+    {
+        [Tooltip("Reference to an Animator component in a game object")]
+        public AnimatorData _animator;
 
-		[Tooltip("Name of the trigger Animator parameter that will be reset")]
-		public StringData _parameterName;
+        [Tooltip("Name of the trigger Animator parameter that will be reset")]
+        public StringData _parameterName;
 
-		public override void OnEnter()
-		{
-			if (_animator.Value != null)
-			{
-				_animator.Value.ResetTrigger(_parameterName);
-			}
+        public override void OnEnter()
+        {
+            if (_animator.Value != null)
+            {
+                _animator.Value.ResetTrigger(_parameterName.Value);
+            }
 
-			Continue();
-		}
+            Continue();
+        }
 
-		public override string GetSummary()
-		{
-			if (_animator.Value == null)
-			{
-				return "Error: No animator selected";
-			}
+        public override string GetSummary()
+        {
+            if (_animator.Value == null)
+            {
+                return "Error: No animator selected";
+            }
 
-			return _animator.Value.name + " (" + _parameterName + ")";
-		}
+            return _animator.Value.name + " (" + _parameterName.Value + ")";
+        }
 
-		public override Color GetButtonColor()
-		{
-			return new Color32(170, 204, 169, 255);
-		}
+        public override Color GetButtonColor()
+        {
+            return new Color32(170, 204, 169, 255);
+        }
 
-		#region Backwards compatibility
+        #region Backwards compatibility
 
-		[HideInInspector] [FormerlySerializedAs("animator")] public Animator animatorOLD;
-		[HideInInspector] [FormerlySerializedAs("parameterName")] public string parameterNameOLD = "";
+        [HideInInspector] [FormerlySerializedAs("animator")] public Animator animatorOLD;
+        [HideInInspector] [FormerlySerializedAs("parameterName")] public string parameterNameOLD = "";
 
-		protected virtual void OnEnable()
-		{
-			if (animatorOLD != null)
-			{
-				_animator.Value = animatorOLD;
-				animatorOLD = null;
-			}
+        protected virtual void OnEnable()
+        {
+            if (animatorOLD != null)
+            {
+                _animator.Value = animatorOLD;
+                animatorOLD = null;
+            }
 
-			if (parameterNameOLD != "")
-			{
-				_parameterName.Value = parameterNameOLD;
-				parameterNameOLD = "";
-			}
-		}
+            if (parameterNameOLD != "")
+            {
+                _parameterName.Value = parameterNameOLD;
+                parameterNameOLD = "";
+            }
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 
 }
