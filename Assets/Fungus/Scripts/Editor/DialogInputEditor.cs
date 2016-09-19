72d1f1d6d8e53da153c6a2c18b970cacf3d97e41
Christopher <chrisgregan@gmail.com>
2016-09-19 20:28:16 +0100

    Added missing Fungus namespace

--- Assets/Fungus/Scripts/Editor/DialogInputEditor.cs
+++ Assets/Fungus/Scripts/Editor/DialogInputEditor.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/// <summary>
-/// Dummy class to avoid compile errors for users upgrading from Fungus 2 to Fungus 3 via unitypackage.
-/// </summary>
-public class DialogInputEditor
-{}
+namespace Fungus
+{
+    /// <summary>
+    /// Dummy class to avoid compile errors for users upgrading from Fungus 2 to Fungus 3 via unitypackage.
+    /// </summary>
+    public class DialogInputEditor
+    {}
+}
