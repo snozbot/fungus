88192856a95fbb2961d6c0368147fd597b01d512
Christopher <chrisgregan@gmail.com>
2016-09-19 13:43:08 +0100

    Fixed missing Process class error in Unity5.5b3

--- Assets/Fungus/Thirdparty/FungusLua/Scripts/LuaEnvironment.cs
+++ Assets/Fungus/Thirdparty/FungusLua/Scripts/LuaEnvironment.cs
@@ -5,6 +5,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System;
+using System.Diagnostics;
 using System.Linq;
 using MoonSharp.Interpreter;
 using MoonSharp.Interpreter.Loaders;
