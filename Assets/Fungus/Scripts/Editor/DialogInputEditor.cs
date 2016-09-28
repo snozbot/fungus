6bb24118daa19a126ff9c729a41eb03956c6f60c
Christopher <chrisgregan@gmail.com>
2016-09-28 14:37:28 +0100

    Added missing copyright headers

--- Assets/Fungus/Scripts/Editor/DialogInputEditor.cs
+++ Assets/Fungus/Scripts/Editor/DialogInputEditor.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿// This code is part of the Fungus library (http://fungusgames.com) maintained by Chris Gregan (http://twitter.com/gofungus).
+// It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
 
 namespace Fungus.EditorUtils
 {
