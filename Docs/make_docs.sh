c693f2a6164a6d88557bf8c8f004574e4d7b9f48
Christopher <chrisgregan@gmail.com>
2016-10-12 10:19:15 +0100

    Make docs build script copies docs to github pages repo

--- Docs/make_docs.sh
+++ Docs/make_docs.sh
@@ -2,3 +2,4 @@
 rm -rf ../Build/Docs
 cp ../Assets/Fungus/Docs/CHANGELOG.txt fungus_docs/change_log.md
 /Applications/Doxygen.app/Contents/Resources/doxygen ./Doxyfile
+cp -R ../Build/Docs/* ../../snozbot.github.io
