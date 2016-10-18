21b15329c6531e77365c4603b25a69f0d38245ba
Christopher <chrisgregan@gmail.com>
2016-10-18 15:48:24 +0100

    Added WaitForFrames property to GameStart event handler. Default is wait for 1 frame (to reduce startup order issues).

--- Assets/Fungus/Scripts/EventHandlers/GameStarted.cs
+++ Assets/Fungus/Scripts/EventHandlers/GameStarted.cs
@@ -2,6 +2,7 @@
 // It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
 
 ﻿using UnityEngine;
+using System.Collections;
 
 namespace Fungus
 {
@@ -13,9 +14,19 @@ namespace Fungus
                       "The block will execute when the game starts playing.")]
     [AddComponentMenu("")]
     public class GameStarted : EventHandler
-    {   
-        protected virtual void Start()
+    {
+        [Tooltip("Wait for a number of frames after startup before executing the Block. Can help fix startup order issues.")]
+        [SerializeField] protected int waitForFrames = 1;
+
+        protected virtual IEnumerator Start()
         {
+            int frameCount = waitForFrames;
+            while (frameCount > 0)
+            {
+                yield return new WaitForEndOfFrame();
+                frameCount--;
+            }
+
             ExecuteBlock();
         }
     }
