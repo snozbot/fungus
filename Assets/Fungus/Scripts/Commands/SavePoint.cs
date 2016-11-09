b9da6f145ef47e5ae3ae1bfe0807a4eb1c9ffd54
Christopher <chrisgregan@gmail.com>
2016-11-09 13:34:50 +0000

    Added SavePointData class

--- Assets/Fungus/Scripts/Commands/SavePoint.cs
+++ Assets/Fungus/Scripts/Commands/SavePoint.cs
@@ -7,47 +7,6 @@ using UnityEngine.SceneManagement;
 
 namespace Fungus
 {
-	[System.Serializable]
-	public class StringVar
-	{
-		public string key;
-		public string value;
-	}
-
-	[System.Serializable]
-	public class IntVar
-	{
-		public string key;
-		public int value;
-	}
-
-	[System.Serializable]
-	public class FloatVar
-	{
-		public string key;
-		public float value;
-	}
-
-	[System.Serializable]
-	public class BoolVar
-	{
-		public string key;
-		public bool value;
-	}
-
-	[System.Serializable]
-	public class SaveData
-	{
-		public string sceneName;
-		public string flowchartName;
-		public string blockName;
-
-		public List<StringVar> stringVars = new List<StringVar>();
-		public List<IntVar> intVars = new List<IntVar>();
-		public List<FloatVar> floatVars = new List<FloatVar>();
-		public List<BoolVar> boolVars = new List<BoolVar>();
-	}
-
 	[CommandInfo("Variable", 
 				 "Save Point", 
 				 "Saves current Flowchart state.")]
@@ -63,7 +22,7 @@ namespace Fungus
 
 		protected string CreateSaveData()
 		{
-			var saveData = new SaveData();
+			var saveData = new SavePointData();
 
 			var flowchart = GetFlowchart();
 
---
+++ Assets/Fungus/Scripts/Utils/SavePointData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace Fungus
+{
+	[System.Serializable]
+	public class StringVar
+	{
+		public string key;
+		public string value;
+	}
+
+	[System.Serializable]
+	public class IntVar
+	{
+		public string key;
+		public int value;
+	}
+
+	[System.Serializable]
+	public class FloatVar
+	{
+		public string key;
+		public float value;
+	}
+
+	[System.Serializable]
+	public class BoolVar
+	{
+		public string key;
+		public bool value;
+	}
+
+	[System.Serializable]
+	public class SavePointData
+	{
+		public string sceneName;
+		public string flowchartName;
+		public string blockName;
+
+		public List<StringVar> stringVars = new List<StringVar>();
+		public List<IntVar> intVars = new List<IntVar>();
+		public List<FloatVar> floatVars = new List<FloatVar>();
+		public List<BoolVar> boolVars = new List<BoolVar>();
+	}
+}
