b650b80cf5383c58cd44eb020a7bb023b604876e
Christopher <chrisgregan@gmail.com>
2017-01-10 15:58:03 +0000

    If Save Point Key is empty use the parent Block Name. If Save Point Description is empty use the current date and time.

--- Assets/Fungus/Scripts/Commands/SavePoint.cs
+++ Assets/Fungus/Scripts/Commands/SavePoint.cs
@@ -7,14 +7,14 @@ namespace Fungus
 {
     [CommandInfo("Flow",
                  "Save Point", 
-                 "Creates a save point which can be saved to persistant storage and loaded again later.")]
+                 "Creates a Save Point and adds it to the Save History. The player can save the Save History to peristent storage and load it again later.")]
     public class SavePoint : Command
     {
-        [Tooltip("A string key which identifies this save point. Must be unique in this scene.")]
-        [SerializeField] protected string savePointKey;
+        [Tooltip("A string key which uniquely identifies this save point. If empty then the parent Block's name will be used.")]
+        [SerializeField] protected string savePointKey = "";
 
-        [Tooltip("A short description of this save point.")]
-        [SerializeField] protected StringData savePointDescription;
+        [Tooltip("A short description of this save point. If empty then the current time and date will be used.")]
+        [SerializeField] protected string savePointDescription;
 
         [Tooltip("Fire an event that will be handled by any Save Point Loaded event handler with matching key.")]
         [SerializeField] protected bool fireEvent = true;
@@ -25,9 +25,36 @@ namespace Fungus
         #region Public members
 
         /// <summary>
-        /// A string key which identifies this save point. Must be unique in this scene.
+        /// A string key which uniquely identifies this Save Point.
+        /// If the save point key is empty then the parent Block's name is returned.
         /// </summary>
-        public string SavePointKey { get { return savePointKey; } }
+        public string SavePointKey 
+        { 
+            get 
+            { 
+                if (string.IsNullOrEmpty(savePointKey))
+                {
+                    return ParentBlock.BlockName;
+                }
+                return savePointKey; 
+            } 
+        }
+
+        /// <summary>
+        /// Gets the save point description.
+        /// If the description is empty then the current time and date will be returned.
+        /// </summary>
+        public string SavePointDescription
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(savePointDescription))
+                {
+                    return System.DateTime.UtcNow.ToString("HH:mm dd MMMM, yyyy");
+                }
+                return savePointDescription; 
+            }
+        }
 
         /// <summary>
         /// Resume execution from this command after loading a save point.
@@ -36,15 +63,9 @@ namespace Fungus
 
         public override void OnEnter()
         {
-            if (string.IsNullOrEmpty(savePointKey))
-            {
-                Continue();
-                return;
-            }
-
             var saveManager = FungusManager.Instance.SaveManager;
 
-            saveManager.AddSavePoint(savePointKey, savePointDescription.Value);
+            saveManager.AddSavePoint(SavePointKey, SavePointDescription);
 
             if (fireEvent)
             {
@@ -58,10 +79,10 @@ namespace Fungus
         {
             if (string.IsNullOrEmpty(savePointKey))
             {
-                return "Error: Save Point Key not specified";
+                return "<Block Name>";
             }
 
-            return savePointKey + " : " + savePointDescription.Value;
+            return savePointKey;
         }
 
         public override Color GetButtonColor()
