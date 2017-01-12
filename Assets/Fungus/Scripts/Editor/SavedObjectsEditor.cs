bd12624b587d762692586858c8fba337515c62e1
Christopher <chrisgregan@gmail.com>
2017-01-12 10:24:30 +0000

    SavedObjects displays flowcharts as a reorderable list.

---
+++ Assets/Fungus/Scripts/Editor/SavedObjectsEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Rotorz.ReorderableList;
+
+namespace Fungus.EditorUtils
+{
+    [CustomEditor (typeof(SavedObjects), true)]
+    public class SavedObjectsEditor : Editor 
+    {
+        protected SerializedProperty flowchartsProp;
+
+        protected virtual void OnEnable()
+        {
+            flowchartsProp = serializedObject.FindProperty("flowcharts");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            ReorderableListGUI.Title("Flowcharts");
+            ReorderableListGUI.ListField(flowchartsProp);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
