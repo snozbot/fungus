819b6c52c76263543518ade0a6317c5874cf0738
Christopher <chrisgregan@gmail.com>
2017-01-26 11:08:48 +0000

    Added custom editor for Save Point Loaded event handler

---
+++ Assets/Fungus/Scripts/Editor/SavePointLoadedEditor.cs
@@ -0,0 +1,30 @@
+﻿// This code is part of the Fungus library (http://fungusgames.com) maintained by Chris Gregan (http://twitter.com/gofungus).
+// It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
+
+#if UNITY_5_3_OR_NEWER
+
+using UnityEngine;
+using UnityEditor;
+using Rotorz.ReorderableList;
+
+namespace Fungus.EditorUtils
+{
+    [CustomEditor (typeof(SavePointLoaded), true)]
+    public class SavePointLoadedEditor : EventHandlerEditor 
+    {
+        protected SerializedProperty savePointKeysProp;
+
+        protected virtual void OnEnable()
+        {
+            savePointKeysProp = serializedObject.FindProperty("savePointKeys");
+        }
+
+        protected override void DrawProperties()
+        {
+            ReorderableListGUI.Title("Save Point Keys");
+            ReorderableListGUI.ListField(savePointKeysProp);
+        }
+    }
+}
+
+#endif
--- Assets/Fungus/Scripts/EventHandlers/SavePointLoaded.cs
+++ Assets/Fungus/Scripts/EventHandlers/SavePointLoaded.cs
@@ -8,7 +8,7 @@ namespace Fungus
 {
     [EventHandlerInfo("Scene",
                       "Save Point Loaded",
-        "Execute this block when a saved point is loaded. Use the 'new_game' key to handle game start.")]
+                      "Execute this block when a saved point is loaded. Use the 'new_game' key to handle game start.")]
     public class SavePointLoaded : EventHandler 
     {
         [Tooltip("Block will execute if the Save Key of the loaded save point matches this save key.")]
