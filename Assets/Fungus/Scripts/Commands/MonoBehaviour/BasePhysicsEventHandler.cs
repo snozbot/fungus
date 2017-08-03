c8ecbce07923bc04b4697fe7a86812dcaeedfbe7
desktop-maesty/steve <halliwell.steve@gmail.com>
2017-08-03 20:10:12 +1000

    Added MonoBeh fungus event handlers and physics demo scene

---
+++ Assets/Fungus/Scripts/Commands/MonoBehaviour/BasePhysicsEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fungus
+{
+
+    /// <summary>
+    /// Base class for all of our physics event handlers
+    /// </summary>
+    [AddComponentMenu("")]
+    public abstract class BasePhysicsEventHandler : TagFilteredEventHandler
+    {
+        [System.Flags]
+        public enum PhysicsMessageType
+        {
+            Enter = 1 << 0,
+            Stay = 1 << 1,
+            Exit = 1 << 2,
+        }
+
+        [Tooltip("Which of the 3d physics messages to we trigger on.")]
+        [SerializeField]
+        [EnumFlag]
+        protected PhysicsMessageType FireOn = PhysicsMessageType.Enter;
+
+        protected void ProcessCollider(PhysicsMessageType from, string tagOnOther)
+        {
+            if ((from & FireOn) != 0)
+            {
+                ProcessTagFilter(tagOnOther);
+            }
+        }
+    }
+}
---
+++ Assets/Fungus/Scripts/Commands/MonoBehaviour/Trigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fungus
+{
+    /// <summary>
+    /// The block will execute when a 3d physics trigger matching some basic conditions is met. 
+    /// </summary>
+    [EventHandlerInfo("MonoBehaviour",
+                      "Trigger",
+                      "The block will execute when a 3d physics trigger matching some basic conditions is met.")]
+    [AddComponentMenu("")]
+    public class Trigger : BasePhysicsEventHandler
+    {
+        private void OnTriggerEnter(Collider col)
+        {
+            ProcessCollider(PhysicsMessageType.Enter, col.tag);
+        }
+
+        private void OnTriggerStay(Collider col)
+        {
+            ProcessCollider(PhysicsMessageType.Stay, col.tag);
+        }
+
+        private void OnTriggerExit(Collider col)
+        {
+            ProcessCollider(PhysicsMessageType.Exit, col.tag);
+        }
+    }
+}
