eacbdff63f82c43b9a31c729ecd32d333c69015d
Chris Gregan <chrisgregan@users.noreply.github.com>
2017-08-22 08:13:35 +0100

    Fixed Unity Test Tools running in Unity 2017.1

--- Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/GuiHelper.cs
+++ Assets/UnityTestTools/IntegrationTestsFramework/TestRunner/Editor/GuiHelper.cs
@@ -13,19 +13,20 @@ namespace UnityTest
     {
         public static bool GetConsoleErrorPause()
         {
-            Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
-            Type type = assembly.GetType("UnityEditorInternal.LogEntries");
-            PropertyInfo method = type.GetProperty("consoleFlags");
-            var result = (int)method.GetValue(new object(), new object[] { });
-            return (result & (1 << 2)) != 0;
+			return false;
+//            Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+//            Type type = assembly.GetType("UnityEditorInternal.LogEntries");
+//            PropertyInfo method = type.GetProperty("consoleFlags");
+//            var result = (int)method.GetValue(new object(), new object[] { });
+//            return (result & (1 << 2)) != 0;
         }
 
         public static void SetConsoleErrorPause(bool b)
         {
-            Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
-            Type type = assembly.GetType("UnityEditorInternal.LogEntries");
-            MethodInfo method = type.GetMethod("SetConsoleFlag");
-            method.Invoke(new object(), new object[] { 1 << 2, b });
+//            Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+//            Type type = assembly.GetType("UnityEditorInternal.LogEntries");
+//            MethodInfo method = type.GetMethod("SetConsoleFlag");
+//            method.Invoke(new object(), new object[] { 1 << 2, b });
         }
     }
 }
