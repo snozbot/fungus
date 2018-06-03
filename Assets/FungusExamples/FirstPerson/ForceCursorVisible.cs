d443cfcacba4cf7bb87e798c6ef726029eb0e344
desktop-maesty/steve <halliwell.steve@gmail.com>
2018-06-03 19:02:13 +1000

    Crude first pass at FPS with Says (blocking and non blocking), and Menus -Added Google Poly Assets

---
+++ Assets/FungusExamples/FirstPerson/ForceCursorVisible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Helper for FirstPerson Fungus Demo to work around some of the inflexibility of the Unity standard fps controller
+/// </summary>
+public class ForceCursorVisible : MonoBehaviour
+{
+    void Update()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
