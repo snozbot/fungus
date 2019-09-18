c11baea29cd786ca2b52ba1ee18e933b69f6c799
Matthew Barnes <taking1n1@gmail.com>
2019-09-18 16:14:39 +0100

    Added a custom script importer for lua files
    Creates text assets out of lua file contents

---
+++ Assets/Fungus/Scripts/Editor/LuaScriptedImporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor.Experimental.AssetImporters;
+using System.IO;
+
+[ScriptedImporter(1, "lua")]
+public class LuaScriptedImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        TextAsset lua = new TextAsset(File.ReadAllText(ctx.assetPath));
+        ctx.AddObjectToAsset("main", lua);
+        ctx.SetMainObject(lua);
+    }
+}
