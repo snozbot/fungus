c26b48f4541e19c04ce059a1f949d5088d28e74f
desktop-maesty/steve <halliwell.steve@gmail.com>
2019-10-29 22:00:40 +1000

    Remove previous doco, now lives in the github wiki Correct Fungus Priority Category to remove space Added Convert To Github wiki format function to Tools menu Export of reference docs moved out of BlockEditor.cs

--- Docs/make_docs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ../Build/Docs
-cp ../Assets/Fungus/Docs/CHANGELOG.txt fungus_docs/change_log.md
-/Applications/Doxygen.app/Contents/Resources/doxygen ./Doxyfile
-cp -R ../Build/Docs/* ../../snozbot.github.io
