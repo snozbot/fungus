25368e728cd15bb0fac2543bca4a59091009a6a6
desktop-maesty/steve <halliwell.steve@gmail.com>
2019-10-31 21:02:28 +1000

    Show all IBlockCallers in BlockEditor

---
+++ Assets/Fungus/Scripts/Interfaces/IBlockCaller.cs
@@ -0,0 +1,12 @@
+﻿namespace Fungus
+{
+    /// <summary>
+    /// Interface for indicating that the class holds a reference to and may call a block
+    /// </summary>
+    public interface IBlockCaller
+    {
+        bool MayCallBlock(Block block);
+
+        string GetLocationIdentifier();
+    }
+}
--- Assets/Fungus/Scripts/Utils/BlockReference.cs
+++ Assets/Fungus/Scripts/Utils/BlockReference.cs
@@ -9,6 +9,9 @@ namespace Fungus
     /// This is the recommended way to directly reference a fungus block in external c# scripts,
     /// as it will give you an inspector field that gives a drop down of all the blocks on a 
     /// flowchart, in a similar way to what you would expect from selecting a block on a command.
+    /// 
+    /// If you want to showup in the Callers section of the block, ensure your monobehaviours 
+    /// that have these also implement IBlockCaller.
     /// </summary>
     [System.Serializable]
     public struct BlockReference
