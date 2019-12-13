80c337335b49832c66282d3268e74601624ce991
desktop-maesty/steve <halliwell.steve@gmail.com>
2019-12-14 09:53:55 +1000

    Added CollectionRandom commands -added simple use to CollectionLoop demo scene

---
+++ Assets/Fungus/Scripts/Commands/Collection/CollectionRandom.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Fungus
+{
+    [CommandInfo("Collection",
+                       "RandomItem",
+                       "Use the collection as a source of random selection. Picking a random item each run.")]
+    [AddComponentMenu("")]
+    public class CollectionRandom : CollectionBaseVarCommand
+    {
+        protected override void OnEnterInner()
+        {
+            collection.Value.Get(Random.Range(0, collection.Value.Count-1), ref variableToUse);
+        }
+    }
+}
