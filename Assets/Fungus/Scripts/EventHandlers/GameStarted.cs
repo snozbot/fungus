4af9f920de58175eed136792a381078fd7b0ad7d
Steve Halliwell <halliwell.steve@gmail.com>
2020-09-08 17:32:19 +1000

    Update SaveGame demos Prefab scripts

--- Assets/Fungus/Scripts/EventHandlers/GameStarted.cs
+++ Assets/Fungus/Scripts/EventHandlers/GameStarted.cs
@@ -18,8 +18,14 @@ namespace Fungus
         [Tooltip("Wait for a number of frames after startup before executing the Block. Can help fix startup order issues.")]
         [SerializeField] protected int waitForFrames = 1;
 
+        [Tooltip("If false, this event will not fire if the game started due to a save being loaded.")]
+        [SerializeField] protected bool shouldExecuteDuringSaveLoad = true;
+
         protected virtual void Start()
         {
+            if (!shouldExecuteDuringSaveLoad && FungusManager.Instance.SaveManager.IsSaveLoading)
+                return;
+
             StartCoroutine(GameStartCoroutine());
         }
 
