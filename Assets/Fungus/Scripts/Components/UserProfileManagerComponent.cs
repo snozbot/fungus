8bf3d0a492ff1385d7eafaf9c34284fa5ffe50b5
Steve Halliwell <halliwell.steve@gmail.com>
2020-10-13 20:06:35 +1000

    Add Lua demo for Save

--- Assets/Fungus/Scripts/Components/UserProfileManagerComponent.cs
+++ Assets/Fungus/Scripts/Components/UserProfileManagerComponent.cs
@@ -1,13 +1,7 @@
 ﻿// This code is part of the Fungus library (https://github.com/snozbot/fungus)
 // It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
 
-using MoonSharp.Interpreter.Tree.Statements;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Linq.Expressions;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Fungus
 {
