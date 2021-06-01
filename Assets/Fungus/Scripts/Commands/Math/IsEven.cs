2871baf9302bfb374266f885c11305c36c705ff5
Michael Cox <michaelwcox07@gmail.com>
2021-06-01 09:09:20 -0500

    Added Modulus command.
    * The Modulus command runs the same operation as x % y == z. This will allow checking whole numbers and other such fun operations like checking list indexes.
    
    * Added an Is Even conditional command that checks an inputted variable and returns true if the number is even.

---
+++ Assets/Fungus/Scripts/Commands/Math/IsEven.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// If the modulus is true, execute the following command block.
+    /// </summary>
+    [CommandInfo("Math",
+                 "Is Even",
+                 "Returns true if the input number is even.")]
+    [AddComponentMenu("")]
+    public class IsEven : Condition
+    {
+        [Tooltip("Variable to check.")]
+        [VariableProperty(typeof(IntegerVariable),
+                          typeof(FloatVariable))]
+        [SerializeField] protected Variable variable;
+        float DivideBy = 2;
+        float remainder = 0;
+
+        public override string GetSummary()
+        {
+            if (variable != null) return "Is (" + variable.Key + ") " + variable.GetValue() + " even?";
+            else return "Error: [Null Variable]";
+        }
+
+        protected override bool EvaluateCondition()
+        {
+            if (variable is IntegerVariable) return (int)variable.GetValue() % DivideBy == remainder;
+            else return (float)variable.GetValue() % DivideBy == remainder;
+        }
+
+        public override bool HasReference(Variable Variable)
+        {
+            return variable == Variable;
+        }
+    }
+}
---
+++ Assets/Fungus/Scripts/Commands/Math/Modulus.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Fungus
+{
+    /// <summary>
+    /// If the modulus is true, execute the following command block.
+    /// </summary>
+    [CommandInfo("Math",
+                 "Modulus",
+                 "Performs a modulus operation on Variable and outputs it to Remainder.")]
+    [AddComponentMenu("")]
+    public class Modulus : Command
+    {
+        [Tooltip("Variable to check.")]
+        [VariableProperty(typeof(IntegerVariable),
+                          typeof(FloatVariable))]
+        [SerializeField] protected Variable variable;
+
+        [SerializeField] protected FloatData DivideBy = new FloatData(2);
+
+        [VariableProperty(typeof(IntegerVariable),
+                          typeof(FloatVariable))]
+        [SerializeField] protected Variable remainderOutput;
+
+        public override void OnEnter()
+        {
+            float value = 0;
+            Flowchart flowchart = GetFlowchart();
+
+            if (variable is IntegerVariable) value = (int)variable.GetValue() % DivideBy.Value;
+            else value = (float)variable.GetValue() % DivideBy.Value;
+
+            if (remainderOutput is IntegerVariable) flowchart.SetIntegerVariable(remainderOutput.Key, System.Convert.ToInt32(value));
+            else flowchart.SetFloatVariable(remainderOutput.Key, value);
+        }
+
+        public override string GetSummary()
+        {
+            if (variable == null) return "Error: [Null Variable] % " + DivideBy;
+            else if (remainderOutput == null) return "Error: [Null remainder output]";
+            else return "(" + variable.Key + ") " + variable.GetValue() + " % " + DivideBy.Value + " -> " + remainderOutput.Key;
+        }
+
+        public override bool HasReference(Variable Variable)
+        {
+            return variable == Variable || DivideBy.floatRef == Variable || remainderOutput == Variable;
+        }
+    }
+}
