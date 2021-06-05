ab97b4bc5efb11ef4ac1885ac51a63a9510b50a8
Steve Halliwell <halliwell.steve@gmail.com>
2021-06-06 07:21:57 +1000

    Merge pull request #971 from ongjinwen/feature/FixDuplicateBug
    Fix double duplication of blocks with Ctrl+D

--- Assets/Fungus/Scripts/Editor/FlowchartWindow.cs
+++ Assets/Fungus/Scripts/Editor/FlowchartWindow.cs
@@ -1895,11 +1895,8 @@ namespace Fungus.EditorUtils
         {
             copyList.Clear();
 
-            foreach (var block in flowchart.SelectedBlocks)
-            {
-                copyList.Add(new BlockCopy(block));
-            }
-            foreach (var block in mouseDownSelectionState)
+            foreach (var block in flowchart.SelectedBlocks
+                .Union(mouseDownSelectionState))
             {
                 copyList.Add(new BlockCopy(block));
             }
