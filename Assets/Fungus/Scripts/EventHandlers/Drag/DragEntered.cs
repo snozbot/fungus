cb21f31f9e65722677be86523ac5ba9e47b8d1c6
CG-Tespy <gbcabreracgt@gmail.com>
2021-06-17 06:17:04 -0400

    [Bugfix] Drag Event Handlers Executing for Draggables not Really Being Dragged (#968)
    * Draggables are now only treated as dragged based on the state of a bool
    
    Before, when a draggable is being moved even in the editor (or by other things like scroll rects), they can still invoke events. With this fix, we can have the events only execute when they actually are being dragged, be it by the user or some code meant to do said dragging
    
    * Moved Drag event handlers to their own folder

--- Assets/Fungus/Scripts/EventHandlers/Drag/DragEntered.cs
+++ Assets/Fungus/Scripts/EventHandlers/Drag/DragEntered.cs
@@ -7,25 +7,27 @@ using UnityEngine;
 namespace Fungus
 {
     /// <summary>
-    /// The block will execute when the player drags an object and successfully drops it on a target object.
+    /// The block will execute when the player is dragging an object which starts touching the target object.
     ///
     /// ExecuteAlways used to get the Compatibility that we need, use of ISerializationCallbackReceiver is error prone
     /// when used on Unity controlled objects as it runs on threads other than main thread.
     /// </summary>
     [EventHandlerInfo("Sprite",
-                      "Drag Completed",
-                      "The block will execute when the player drags an object and successfully drops it on a target object.")]
+                      "Drag Entered",
+                      "The block will execute when the player is dragging an object which starts touching the target object.")]
     [AddComponentMenu("")]
     [ExecuteInEditMode]
-    public class DragCompleted : EventHandler, ISerializationCallbackReceiver
+    public class DragEntered : EventHandler, ISerializationCallbackReceiver
     {
-        public class DragCompletedEvent
+        public class DragEnteredEvent
         {
             public Draggable2D DraggableObject;
+            public Collider2D TargetCollider;
 
-            public DragCompletedEvent(Draggable2D draggableObject)
+            public DragEnteredEvent(Draggable2D draggableObject, Collider2D targetCollider)
             {
                 DraggableObject = draggableObject;
+                TargetCollider = targetCollider;
             }
         }
 
@@ -47,12 +49,6 @@ namespace Fungus
 
         [SerializeField] protected List<Collider2D> targetObjects;
 
-        // There's no way to poll if an object is touching another object, so
-        // we have to listen to the callbacks and track the touching state ourselves.
-        protected bool overTarget = false;
-
-        protected Collider2D targetCollider = null;
-
         protected EventDispatcher eventDispatcher;
 
         protected virtual void OnEnable()
@@ -61,14 +57,7 @@ namespace Fungus
             {
                 eventDispatcher = FungusManager.Instance.EventDispatcher;
 
-                eventDispatcher.AddListener<DragCompletedEvent>(OnDragCompletedEvent);
-                eventDispatcher.AddListener<DragEntered.DragEnteredEvent>(OnDragEnteredEvent);
-                eventDispatcher.AddListener<DragExited.DragExitedEvent>(OnDragExitedEvent);
-
-                foreach (Draggable2D dragObj in draggableObjects)
-                {
-                    dragObj.RegisterHandler(this);
-                }
+                eventDispatcher.AddListener<DragEnteredEvent>(OnDragEnteredEvent);
             }
         }
 
@@ -76,39 +65,21 @@ namespace Fungus
         {
             if (Application.isPlaying)
             {
-                eventDispatcher.RemoveListener<DragCompletedEvent>(OnDragCompletedEvent);
-                eventDispatcher.RemoveListener<DragEntered.DragEnteredEvent>(OnDragEnteredEvent);
-                eventDispatcher.RemoveListener<DragExited.DragExitedEvent>(OnDragExitedEvent);
-
-                foreach (Draggable2D dragObj in draggableObjects)
-                {
-                    dragObj.UnregisterHandler(this);
-                }
+                eventDispatcher.RemoveListener<DragEnteredEvent>(OnDragEnteredEvent);
 
                 eventDispatcher = null;
             }
         }
 
-        private void OnDragCompletedEvent(DragCompletedEvent evt)
-        {
-            OnDragCompleted(evt.DraggableObject);
-        }
-
-        private void OnDragEnteredEvent(DragEntered.DragEnteredEvent evt)
+        private void OnDragEnteredEvent(DragEnteredEvent evt)
         {
             OnDragEntered(evt.DraggableObject, evt.TargetCollider);
-        }
-
-        private void OnDragExitedEvent(DragExited.DragExitedEvent evt)
-        {
-            OnDragExited(evt.DraggableObject, evt.TargetCollider);
         }
 
         #region Compatibility
 
         void ISerializationCallbackReceiver.OnAfterDeserialize()
         {
-            //presentl using awake due to errors on non main thread access of targetCollider
         }
 
         void ISerializationCallbackReceiver.OnBeforeSerialize()
@@ -142,69 +113,23 @@ namespace Fungus
         #region Public members
 
         /// <summary>
-        /// Gets the draggable object.
-        /// </summary>
-        public virtual List<Draggable2D> DraggableObjects { get { return draggableObjects; } }
-
-        /// <summary>
-        /// Returns true if the draggable object is over the drag target object.
-        /// </summary>
-        public virtual bool IsOverTarget()
-        {
-            return overTarget;
-        }
-
-        /// <summary>
-        /// Called by the Draggable2D object when the it enters the drag target.
+        /// Called by the Draggable2D object when the the drag enters the drag target.
         /// </summary>
         public virtual void OnDragEntered(Draggable2D draggableObject, Collider2D targetObject)
         {
-            if (this.targetObjects != null && this.draggableObjects != null &&
+            if (draggableObject.BeingDragged &&
+                this.targetObjects != null && this.draggableObjects != null &&
                 this.draggableObjects.Contains(draggableObject) &&
                 this.targetObjects.Contains(targetObject))
             {
-                overTarget = true;
-                targetCollider = targetObject;
-            }
-        }
-
-        /// <summary>
-        /// Called by the Draggable2D object when the it exits the drag target.
-        /// </summary>
-        public virtual void OnDragExited(Draggable2D draggableObject, Collider2D targetObject)
-        {
-            if (this.targetObjects != null && this.draggableObjects != null &&
-                this.draggableObjects.Contains(draggableObject) &&
-                this.targetObjects.Contains(targetObject))
-            {
-                overTarget = false;
-                targetCollider = null;
-            }
-        }
-
-        /// <summary>
-        /// Called by the Draggable2D object when the the drag ends over the drag target.
-        /// </summary>
-        public virtual void OnDragCompleted(Draggable2D draggableObject)
-        {
-            if (this.draggableObjects.Contains(draggableObject) &&
-                overTarget)
-            {
-                // Assume that the player will have to do perform another drag and drop operation
-                // to complete the drag again. This is necessary because we don't get an OnDragExited if the
-                // draggable object is set to be inactive.
                 if (draggableRef != null)
                 {
                     draggableRef.Value = draggableObject.gameObject;
                 }
                 if (targetRef != null)
                 {
-                    targetRef.Value = targetCollider.gameObject;
+                    targetRef.Value = targetObject.gameObject;
                 }
-
-                overTarget = false;
-                targetCollider = null;
-
                 ExecuteBlock();
             }
         }
