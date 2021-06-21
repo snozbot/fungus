09552579f567accc5bcd16180ef932f387b98572
breadnone <64100867+breadnone@users.noreply.github.com>
2021-06-21 17:20:52 +0700

    Character Editor UI Refresh
    CharacterEditor UI Refresh
    
    Character Editor New UI

--- Assets/Fungus/Scripts/Editor/CharacterEditor.cs
+++ Assets/Fungus/Scripts/Editor/CharacterEditor.cs
@@ -18,6 +18,9 @@ namespace Fungus.EditorUtils
         protected SerializedProperty setSayDialogProp;
         protected SerializedProperty effectAudioSourceProp;
         protected SerializedProperty voiceAudioSourceProp;
+        protected bool toggleLock = false;
+        protected bool defaultPortrait = false;
+        protected int counter = 0;
 
         protected virtual void OnEnable()
         {
@@ -50,7 +53,8 @@ namespace Fungus.EditorUtils
             if (t.Portraits != null &&
                 t.Portraits.Count > 0)
             {
-                t.ProfileSprite = t.Portraits[0];
+                if (!defaultPortrait)
+                    t.ProfileSprite = t.Portraits[0];
             }
             else
             {
@@ -65,7 +69,126 @@ namespace Fungus.EditorUtils
                 if (characterTexture != null)
                     GUI.DrawTexture(previewRect,characterTexture,ScaleMode.ScaleToFit,true,aspect);
             }
+            GUILayout.BeginHorizontal("box");
+            GUILayout.FlexibleSpace();
 
+            var myCustomStyle = new GUIStyle(GUI.skin.GetStyle("label"));
+            myCustomStyle.wordWrap = true;
+            myCustomStyle.richText = true;
+            myCustomStyle.normal.textColor = Color.yellow;
+            myCustomStyle.alignment = TextAnchor.MiddleCenter;
+
+            if (GUILayout.Button("PREV", GUILayout.Width(50), GUILayout.Height(30)))
+            {
+                if (t.Portraits != null)
+                {
+                    if (!defaultPortrait)
+                    {
+                        defaultPortrait = true;
+                    }
+                    if (counter != 0)
+                    {
+                        counter--;
+                        if (t.Portraits[counter] != null)
+                            t.ProfileSprite = t.Portraits[counter];
+                    }
+                    if (counter == 0)
+                    {
+                        t.ProfileSprite = t.Portraits[0];
+                    }
+                }
+            }
+
+            if (GUILayout.Button("NEXT", GUILayout.Width(50), GUILayout.Height(30)))
+            {
+                if (t.Portraits != null)
+                {
+                    if (!defaultPortrait)
+                    {
+                        defaultPortrait = true;
+                    }
+                    if (counter != t.Portraits.Count - 1)
+                    {
+                        counter++;
+                        if (t.Portraits[counter] != null)
+                            t.ProfileSprite = t.Portraits[counter];
+                    }
+                }
+            }
+
+            if (GUILayout.Button("DEL", GUILayout.Width(50), GUILayout.Height(30)))
+            {
+                if (t.Portraits != null && t.Portraits.Count > 0)
+                {
+                    if (EditorUtility.DisplayDialog("Delete Character's Portrait", "Are you sure you want to delete this portrait!?", "DELETE", "CANCEL"))
+                    {
+                        t.Portraits.RemoveAt(counter);
+                        t.Portraits.TrimExcess();
+                        t.ProfileSprite = t.Portraits[counter];
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            if (GUILayout.Button("CLEAR", GUILayout.Width(50), GUILayout.Height(30)))
+            {
+                if (t.Portraits != null && t.Portraits.Count > 0)
+                {
+                    if (EditorUtility.DisplayDialog("REMOVE Character Portraits", "Are you sure you want to remove all portraits for this character!?", "REMOVE ALL", "CANCEL"))
+                    {
+                        t.Portraits.Clear();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+            }
+
+            //Lock the Character inspector so selecting multiple files in assets folder will be easier without losing focus
+            if (GUILayout.Button("LOCK", GUILayout.Width(50), GUILayout.Height(30)))
+            {
+                if (!toggleLock)
+                {
+                    toggleLock = true;
+                    ActiveEditorTracker.sharedTracker.isLocked = true;
+                }
+                else
+                {
+                    toggleLock = false;
+                    ActiveEditorTracker.sharedTracker.isLocked = false;
+                }
+            }
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
+            if (t.ProfileSprite != null && t.Portraits.Count > 0)
+            {
+                GUILayout.BeginHorizontal("box");
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("NO : " + counter + " : " + t.Portraits[counter].name);
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+            }
+
+            if (toggleLock)
+            {
+                GUILayout.BeginHorizontal("box");
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("<b><size=15>INSPECTOR IS LOCKED!</size></b>\nPress Lock button to unlock", myCustomStyle);
+                GUILayout.FlexibleSpace();
+                GUILayout.EndHorizontal();
+            }
+
+            //Drag n drop area;
+            EditorGUILayout.Space();
+            DropAreaGUI();
+
+            EditorGUILayout.Separator();
             EditorGUILayout.PropertyField(portraitsProp, new GUIContent("Portraits", "Character image sprites to display in the dialog"), true);
 
             EditorGUILayout.HelpBox("All portrait images should use the exact same resolution to avoid positioning and tiling issues.", MessageType.Info);
@@ -86,6 +209,45 @@ namespace Fungus.EditorUtils
                 EditorUtility.SetDirty(t);
 
             serializedObject.ApplyModifiedProperties();
+        }
+        
+        public void DropAreaGUI()
+        {
+            Character t = target as Character;
+            Event evt = Event.current;
+
+            Rect drop_area = GUILayoutUtility.GetRect(0.0f, 60.0f, GUILayout.ExpandWidth(true));
+            var centeredStyle = GUI.skin.GetStyle("Box");
+            centeredStyle.alignment = TextAnchor.MiddleCenter;
+            centeredStyle.fontStyle = FontStyle.Bold;
+            centeredStyle.richText = true;
+
+            GUI.Box(drop_area, "<size=24><color=green>DRAG & DROP</color></size>\n<color=red>CHARACTER SPRITES HERE</color>");
+            switch (evt.type)
+            {
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    if (!drop_area.Contains(evt.mousePosition))
+                        return;
+
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                    if (evt.type == EventType.DragPerform)
+                    {
+                        DragAndDrop.AcceptDrag();
+                        var path = DragAndDrop.paths;
+                        for (int i = 0; i < DragAndDrop.objectReferences.Length; i++)
+                        {
+                            var afterLoadedSprite = AssetDatabase.LoadAssetAtPath<Sprite>(path[i]);
+
+                            // No need to check for types, if it's not a Sprite it will be nulled by default
+                            if (afterLoadedSprite != null)
+                                t.Portraits.Add(afterLoadedSprite);
+                        }
+                        ActiveEditorTracker.sharedTracker.ForceRebuild();
+                    }
+                    break;
+            }
         }
 
     }
