7c51a0d02b693d9f23f51ff88522ae50c5f1f87f
CG-Tespy <gbcabreracgt@gmail.com>
2021-10-27 22:01:38 -0400

    Organized Flowchart tests, created base for stage tests

---
+++ Assets/Tests/PlayMode/SaveSystem/StageSavingTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace SaveSystemTests
+{
+    public class StageSavingTests
+    {
+
+    }
+}
