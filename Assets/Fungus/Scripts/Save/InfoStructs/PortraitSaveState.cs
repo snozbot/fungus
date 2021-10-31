0c3a26f4be0f3d6f8238e03eafeae14de32366ec
CG-Tespy <gbcabreracgt@gmail.com>
2021-10-31 02:04:15 -0400

    Implemented test for position-saving

--- Assets/Fungus/Scripts/Save/InfoStructs/PortraitSaveState.cs
+++ Assets/Fungus/Scripts/Save/InfoStructs/PortraitSaveState.cs
@@ -9,7 +9,7 @@ namespace Fungus
     /// to be able to restore portraits to what they should be upon load.
     /// </summary>
     [System.Serializable]
-    public class PortraitSaveState
+    public class PortraitSaveState : System.IEquatable<PortraitSaveState>
     {
         public string CharacterName
         {
@@ -73,6 +73,46 @@ namespace Fungus
 
         [SerializeField]
         string portraitName = "[NullPortraitName]"; 
+
+        public static PortraitSaveState From(Character character)
+        {
+            PortraitState charState = character.State;
+            PortraitSaveState newState = new PortraitSaveState();
+
+            newState.characterName = character.name;
+            newState.Dimmed = charState.dimmed;
+            newState.FacingDirection = charState.facing;
+            newState.OnScreen = charState.onScreen;
+            newState.PortraitName = charState.portrait.name;
+            newState.Position = charState.position.position;
+
+            newState.stageName = FindStageNameFor(charState);
+
+            return newState;
+        }
+            
+
+        protected static string FindStageNameFor(PortraitState state)
+        {
+            // The Stage is the portrait holder's grandparent, so we need to hop up two spots
+            // in the Hierarchy
+            Transform portraitHolder = state.holder;
+            Transform canvasHoldingTheHolder = portraitHolder.parent.transform;
+            Stage stage = canvasHoldingTheHolder.parent.GetComponent<Stage>();
+            return stage.name;
+
+        }
+
+        public virtual bool Equals(PortraitSaveState other)
+        {
+            return this.CharacterName == other.CharacterName &&
+                this.Dimmed == other.Dimmed &&
+                this.FacingDirection == other.FacingDirection &&
+                this.OnScreen == other.OnScreen &&
+                this.PortraitName == other.PortraitName &&
+                this.Position == other.Position &&
+                this.StageName == other.StageName;
+        }
 
     }
 }
--- Assets/Tests/PlayMode/SaveSystem/StageSavingTests.cs
+++ Assets/Tests/PlayMode/SaveSystem/StageSavingTests.cs
@@ -16,12 +16,7 @@ namespace SaveSystemTests
         {
             base.SetUp();
             GetPortraitsPrepped();
-        }
-
-        [UnityTest]
-        public IEnumerator WaitForthings()
-        {
-            yield return new WaitForSeconds(5f);
+            RegisterStageAndPositions();
         }
 
         protected virtual void GetPortraitsPrepped()
@@ -30,15 +25,34 @@ namespace SaveSystemTests
             prep.ExecuteBlock("Execute");
         }
 
-        [Test]
-        [Ignore("")]
-        public virtual void PortraitPositionsSaved()
+        protected virtual void RegisterStageAndPositions()
         {
-            // Act
+            stageForPortraits = GameObject.Find("TestStage").GetComponent<Stage>();
+            onTheRight = GameObject.Find("TestRight").transform.position;
+            onTheLeft = GameObject.Find("TestLeft").transform.position;
+        }
 
-            // Set up save data that records the states of all portraits
+        protected Stage stageForPortraits;
+        protected Vector3 onTheRight, onTheLeft;
 
-            // Assert
+        [UnityTest]
+        public virtual IEnumerator PortraitPositionsSaved()
+        {
+            yield return PostSetUp();
+
+            bool yesTheyWereSaved = SherlockSavedAsOnTheRight() && WatsonSavedAsOnTheLeft();
+            Assert.IsTrue(yesTheyWereSaved);
+        }
+
+        /// <summary>
+        /// Every test will need this so they can be sure that the portraits are truly ready
+        /// </summary>
+        /// <returns></returns>
+        protected IEnumerator PostSetUp()
+        {
+            yield return WaitForPortraitPrep();
+            savedPortraitStates = GetStatesOfAllPortraits();
+            yield return null;
         }
 
         protected virtual IEnumerator WaitForPortraitPrep()
@@ -46,7 +60,66 @@ namespace SaveSystemTests
             yield return new WaitForSeconds(portraitPrepTime);
         }
 
-        protected float portraitPrepTime = 2f;
+        protected float portraitPrepTime = 1.5f;
+
+        protected virtual IList<PortraitSaveState> GetStatesOfAllPortraits()
+        {
+            IList<Character> allChars = GameObject.FindObjectsOfType<Character>();
+            IList<PortraitSaveState> states = new List<PortraitSaveState>();
+
+            foreach (var character in allChars)
+            {
+                PortraitSaveState saveState = PortraitSaveState.From(character);
+                states.Add(saveState);
+            }
+
+            return states;
+        }
+
+        protected IList<PortraitSaveState> savedPortraitStates = new List<PortraitSaveState>();
+
+        protected virtual bool SherlockSavedAsOnTheRight()
+        {
+            var sherlockState = GetStateFor("Sherlock Holmes");
+            return sherlockState.Position == onTheRight;
+        }
+
+        protected virtual PortraitSaveState GetStateFor(string charName)
+        {
+            foreach (var savedState in savedPortraitStates)
+                if (savedState.CharacterName == charName)
+                    return savedState;
+
+            throw new System.InvalidOperationException("There is no state registered with " + charName);
+        }
+
+        protected virtual bool WatsonSavedAsOnTheLeft()
+        {
+            var watsonState = GetStateFor("John Watson");
+            return watsonState.Position == onTheLeft;
+        }
+
+
+        protected virtual bool StateListsAreTheSame(IList<PortraitSaveState> firstStates, IList<PortraitSaveState> secondStates)
+        {
+            bool differentContentAmounts = firstStates.Count != secondStates.Count;
+            if (differentContentAmounts)
+                return false;
+
+            int statesToCheck = firstStates.Count;
+            for (int i = 0; i < statesToCheck; i++)
+            {
+                var firstStateEl = firstStates[i];
+                var secondStateEl = secondStates[i];
+
+                bool theyAreTheSame = firstStateEl.Equals(secondStateEl);
+                if (!theyAreTheSame)
+                    return false;
+            }
+
+            return true;
+        }
+        
 
         protected virtual IEnumerator HavePortraitsReady()
         {
