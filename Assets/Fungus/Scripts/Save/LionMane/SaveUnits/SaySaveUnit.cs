b86d056a86ec9f21f8f4c45bffdae8efd4fb1036
CG-Tespy <gbcabreracgt@gmail.com>
2021-11-27 08:18:52 -0500

    Renamed save unit folder, made progress on saving say command execution count

---
+++ Assets/Fungus/Scripts/Save/LionMane/SaveUnits/SaySaveUnit.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Fungus.LionManeSaveSys
+{
+    [System.Serializable]
+    public struct SaySaveUnit : ISaveUnit<SaySaveUnit>, System.IEquatable<SaySaveUnit>
+    {
+        public SaySaveUnit Contents => this;
+        object ISaveUnit.Contents => this;
+
+        public int ExecutionCount
+        {
+            get { return executionCount; }
+            set { executionCount = value; }
+        }
+
+        [SerializeField]
+        int executionCount;
+
+        public bool Equals(SaySaveUnit other)
+        {
+            return this.ExecutionCount == other.ExecutionCount;
+        }
+
+        public static SaySaveUnit From(Say command)
+        {
+            SaySaveUnit newUnit = new SaySaveUnit();
+            newUnit.executionCount = command.ExecutionCount;
+            return newUnit;
+        }
+
+        public static IList<SaySaveUnit> From(IList<Say> commands)
+        {
+            SaySaveUnit[] results = new SaySaveUnit[commands.Count];
+
+            for (int i = 0; i < commands.Count; i++)
+            {
+                Say currentSay = commands[i];
+                SaySaveUnit newUnit = From(currentSay);
+                results[i] = newUnit;
+            }
+
+            return results;
+        }
+    }
+}
