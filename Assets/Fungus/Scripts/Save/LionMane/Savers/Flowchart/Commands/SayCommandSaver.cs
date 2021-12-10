8bd08633b81257486cca17aa364caf09d338a7a6
CG-Tespy <gbcabreracgt@gmail.com>
2021-12-10 15:06:46 -0500

    Code reorganization, started work on SayCommandSaver

---
+++ Assets/Fungus/Scripts/Save/LionMane/Savers/Flowchart/Commands/SayCommandSaver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fungus.LionManeSaveSys
+{
+    public class SayCommandSaver : DataSaver<SaySaveUnit, Block>
+    {
+        public override ISaveUnit CreateSaveFrom(object input)
+        {
+            if (IsValid(input))
+                return CreateSaveFrom(input as Block);
+            else
+                throw new System.ArgumentException("");
+        }
+
+        protected override bool IsValid(object input)
+        {
+            return input is Block;
+        }
+
+        public virtual SaySaveUnit CreateSaveFrom(Block block)
+        {
+
+        }
+
+    }
+}
