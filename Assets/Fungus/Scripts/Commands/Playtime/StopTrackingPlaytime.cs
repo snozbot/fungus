3f6c49737cf783830d006ecd2461be6477eb6b06
CG-Tespy <gbcabreracgt@gmail.com>
2021-12-29 01:56:41 -0500

    Added StopTrackingPlaytime Command

---
+++ Assets/Fungus/Scripts/Commands/Playtime/StopTrackingPlaytime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fungus.PlaytimeSys
+{
+    [CommandInfo("Playtime", "Stop Tracking Playtime", "As it sounds.")]
+    public class StopTrackingPlaytime : Command
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            tracker = FungusManager.Instance.PlaytimeTracker;
+            tracker.StopTracking();
+            Continue();
+        }
+
+        protected PlaytimeTracker tracker;
+
+    }
+}
