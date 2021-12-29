e819956e83986a9b221b2344f5b8210b173211d5
CG-Tespy <gbcabreracgt@gmail.com>
2021-12-29 01:44:11 -0500

    Added StartTrackingPlaytime Command

---
+++ Assets/Fungus/Scripts/Commands/Playtime/StartTrackingPlaytime.cs
@@ -0,0 +1,16 @@
+﻿namespace Fungus.PlaytimeSys
+{
+    [CommandInfo("Playtime", "Start Tracking Playtime", "Just as it sounds. Does nothing if the playtime's already being tracked.")]
+    public class StartTrackingPlaytime : Command
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            tracker = FungusManager.Instance.PlaytimeTracker;
+            tracker.StartTracking();
+            Continue();
+        }
+
+        PlaytimeTracker tracker;
+    }
+}
