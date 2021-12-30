e6e5f84b0be12736f37098ec017fc7d4ffff397c
CG-Tespy <gbcabreracgt@gmail.com>
2021-12-30 05:17:38 -0500

    Added SetTimerMode Command

---
+++ Assets/Fungus/Scripts/Time/Commands/SetTimerMode.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Fungus.TimeSys
+{
+    [CommandInfo("Timer", "Set Timer Mode", "Sets the type of the timer with the given ID")]
+    public class SetTimerMode : TimerCommand
+    {
+        [SerializeField]
+        TimerMode timerMode;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            TimerManager.SetModeOfTimerWithID(timerID, timerMode);
+            Continue();
+        }
+    }
+}
