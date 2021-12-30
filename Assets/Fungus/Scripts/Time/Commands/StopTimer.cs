3066da213360ffbb22fc9dafc5e21c0dbd1a5d7a
CG-Tespy <gbcabreracgt@gmail.com>
2021-12-30 05:05:44 -0500

    Added Commands for stopping and resetting timers

---
+++ Assets/Fungus/Scripts/Time/Commands/StopTimer.cs
@@ -0,0 +1,12 @@
+﻿namespace Fungus.TimeSys
+{
+    [CommandInfo("Timer", "Stop Timer", "Stops the timer with the provided ID. Does nothing if the timer isn't running at the time.")]
+    public class StopTimer : TimerCommand
+    {
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            TimerManager.StopTimerWithID(timerID);
+        }
+    }
+}
