f62062807dac8175a94b079e176753aef2e9264f
CG-Tespy <gbcabreracgt@gmail.com>
2021-12-30 23:02:41 -0500

    Added Timer EventHandlers

---
+++ Assets/Fungus/Scripts/Time/EventHandlers/OnTimerStarted.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Fungus.TimeSys
+{
+    [EventHandlerInfo("Timer", "Timer Started", "Executes this block when the specified timers start.")]
+    public class OnTimerStarted : TimerEventHandler
+    {
+        protected override Action<Timer> TimerEvent
+        {
+            get { return Timer.OnAnyTimerStart; }
+            set { Timer.OnAnyTimerStart = value; }
+        }
+    }
+}
---
+++ Assets/Fungus/Scripts/Time/EventHandlers/TimerEventHandler.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+namespace Fungus.TimeSys
+{
+    
+    public abstract class TimerEventHandler : EventHandler
+    {
+        [SerializeField]
+        [VariableProperty(typeof(IntegerVariable))]
+        [Tooltip("If this has nothing, this responds to any Timer as appropriate for this event handler")]
+        protected IntegerVariable[] timerIDs;
+
+        protected virtual void OnEnable()
+        {
+            ListenForTheTimerEvent();
+        }
+
+        protected virtual void ListenForTheTimerEvent()
+        {
+            TimerEvent += OnTimerEventExecuted;
+        }
+
+        protected abstract System.Action<Timer> TimerEvent { get; set; }
+
+        protected virtual void OnTimerEventExecuted(Timer timerThatStarted)
+        {
+            if (this.ShouldExecuteFor(timerThatStarted))
+                ExecuteBlock();
+        }
+
+        protected virtual bool ShouldExecuteFor(Timer timer)
+        {
+            bool executeForAnyTimer = timerIDs.Length == 0;
+
+            if (executeForAnyTimer)
+                return true;
+
+            return ThisHasIDOf(timer);
+        }
+
+        protected virtual bool ThisHasIDOf(Timer timer)
+        {
+            foreach (var intVar in this.timerIDs)
+            {
+                bool validVar = intVar != null;
+
+                if (!validVar)
+                    continue;
+
+                int id = intVar.Value;
+                if (timer.ID == id)
+                    return true;
+            }
+
+            return false;
+        }
+
+        protected virtual void OnDisable()
+        {
+            UnlistenForTheTimerEvent();
+        }
+
+        protected virtual void UnlistenForTheTimerEvent()
+        {
+            TimerEvent -= OnTimerEventExecuted;
+        }
+
+        
+
+    }
+}
